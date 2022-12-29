5bf1e56e73416625a133aa43c4ea89714b1d1a31
mostafaghidan <87931164+mostafaghidan@users.noreply.github.com>
2022-12-29 14:56:38 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,20 @@
-import { Component, VERSION } from '@angular/core';
+
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
+
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
-  constructor(){}
-  counter =0;
-  increment(){
-    this.counter++
+
+  constructor () {}
+
+  counter = 0;
+
+  increment() {
+    this.counter++;
   }
+  
 }
